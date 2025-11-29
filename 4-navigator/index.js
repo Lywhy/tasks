326d759df0aa703d83f7ce9da34dc8826f569862
Lywhy <123964598+Lywhy@users.noreply.github.com>
2025-11-29 11:38:58 +0300

    fixed few problems

--- 4-navigator/index.js
+++ 4-navigator/index.js
@@ -2,7 +2,7 @@
 const addressLat = 10;
 const addressLong = 12;
 const positionLat = 2;
-const positionLong = '4';
+const positionLong = 4;
 
 function checkParameters(param) {
     return typeof param === 'number';
@@ -13,7 +13,7 @@ function distancePoints(x2, x1, y2, y1) {
     if ([...arguments].every(item => checkParameters(item))) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
-    console.error("Неверный формат входных параметров. Входные параметры должны быть числом!");
+    return "Неверный формат входных параметров. Входные параметры должны быть числом!";
 }
 
 console.log(distancePoints(addressLat, positionLat, addressLong, positionLong));
