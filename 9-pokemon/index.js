05a79b86d383d4eee489bc7fae22a6488ffcb52e
Lywhy <123964598+Lywhy@users.noreply.github.com>
2026-01-22 21:39:13 +0300

    '9-pokemon'

---
+++ 9-pokemon/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const req =  new XMLHttpRequest();
+req.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
+req.send();
+req.addEventListener('load', function() {
+    if (req.status > 199 && req.status < 300) {
+
+        const {abilities} = req.response;
+
+        const reqEn = new XMLHttpRequest();
+        reqEn.open('GET', abilities[0].ability.url);
+        reqEn.send();
+        reqEn.addEventListener('load', function() {
+            if (reqEn.status > 199 && reqEn.status < 300) {
+                const {effect_entries} = reqEn.response;
+                for (const ef of effect_entries) {
+                    if (ef.language.name === 'en') {
+                        console.log(ef.effect)
+                    }
+                }
+            } else {
+                console.error(`Request status error: ${reqEn.status}`)
+            }
+        })
+    } else {
+        console.error(`Request status error: ${req.status}`)
+    }
+})
